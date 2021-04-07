66e6b7a18311fddd4d61ba2b92677c717076a5ed
Humuni <pearlkong@gmail.com>
2021-04-07 17:58:16 -0400

    https://www.youtube.com/watch?v=fcZXfoB2f70&ab_channel=RickrollNoads

--- IT2/app.py
+++ IT2/app.py
@@ -3,8 +3,11 @@ Group 49: Ming Zhu Kong, Khushdip Nijjar, Shantanu Singh, Aston Wright
 
 1. Run app.py
 2. localhost:5000 in browser to access
-'''
 
+*For remote access, on same network*
+3. If local host, cmd -> ipconfig for ipv4 address
+4. In remote browser -> http://<host ipv4 address>:5000/
+'''
 
 #micro web framework in python
 from flask import Flask, render_template, session, g, request, redirect, url_for
@@ -12,13 +15,27 @@ from flask import Flask, render_template, session, g, request, redirect, url_for
 '''additional modules for database implementation and database password encryption down the line'''
 # from werkzeug.security import generate_password_hash, check_password_hash
 import sqlite3
+import random
 
 app = Flask(__name__)
 app.secret_key = 'cps406'.encode('utf-8')
 
+'''hi, no touchy pls'''
+'''------------------------------'''
+# list of all existing security questions, in order with indice
+SECURITY_QUESTIONS = ["What is your mother's maiden name?", "What is the name of your elementary school?", "What the name of your first pet?", "What was your first car?", "What is the name of the town you were born?"]
+# for password recovery only
+RECOVERY_QUESTION = ""
+RECOVERY_ANS = ""
+PASS_RECOVERY = ""
+# for registration only
+BUILD_USER = ""
+'''------------------------------'''
+
+
 ### For Implementation with SQL database down the line ###
 
-# the database file (TBA later)
+# the database file
 DATABASE = './cypressdb.db'
 
 # connect to the database
@@ -78,7 +95,7 @@ def login():
     error = ''
     confirmation = ''
     if request.method == 'POST':
-        sql = 'select full_name, email, password from user_info where email=\'' + request.form['email'] + '\''
+        sql = 'select full_name, email, password, address_line_1, address_line_2, postal_code, phone_number from user_info where email=\'' + request.form['email'] + '\''
         try:
             user = query_db(sql)[0]
         except IndexError:
@@ -86,14 +103,16 @@ def login():
             return render_template('login.html', error=error)
 
         if (user[1] == request.form['email']) and (user[2] == request.form['password']):
-            session['user'] = {'email': request.form['email'], 'fullname': user[0]}
+            session['user'] = {'email': user[1], 'fullname': user[0], 'user_password': user[2], 'address_line_1': user[3], 'address_line_2': user[4], 'postal_code': user[5], 'phone_number': user[6]}
             return render_template('home.html', error=error)
         else:
-            error = 'Incorrect email or password, please try again.'
+            error = 'Incorrect password, please try again.'
     elif 'user' in session:
         return render_template('home.html', error=error)
     elif len(request.args) > 0 and request.args.get('f') == 'register':
-        confirmation = 'Account created successfully! Please log in to access CYPRESS.'
+        confirmation = 'Security answers set successfully! Please log in to access CYPRESS.'
+    elif len(request.args) > 0 and request.args.get('f') == 'failed':
+        confirmation = 'Failed to set security questions. Please log in to access CYPRESS.'
 
     return render_template('login.html', error=error, confirmation=confirmation)
 
@@ -104,28 +123,44 @@ def login():
 @app.route('/register', methods=['GET','POST'])
 def register():
     error = ''
+    global BUILD_USER
+    register_passed = False
     if request.method == 'POST':
-        # missing: check for secure password
-        # missing: check for proper phone number(?) (need some kind of standard format)
-        if request.form['address-2'] == '':
-            sql = ('insert into user_info (full_name, email, password, address_line_1, postal_code, phone_number) values (\'' +
-                    request.form['fullname'] + '\',\'' + request.form['email'] + '\',\'' +
-                    request.form['password'] + '\',\'' + request.form['address-1'] + '\', \'' + request.form['postal'] + '\',\'' + request.form['phone-number'] + '\')')
-        else:
-            sql = ('insert into user_info (full_name, email, password, address_line_1, address_line_2, postal_code, phone_number) values (\'' +
-                    request.form['fullname'] + '\',\'' + request.form['email'] + '\',\'' +
-                    request.form['password'] + '\',\'' + request.form['address-1'] + '\',\'' +
-                    request.form['address-2'] + '\',\'' + request.form['postal'] + '\',\'' + request.form['phone-number'] + '\')')
-        msg = write_db(sql)
+        if 'email' in request.form:
+            # missing: check for secure password
+            # missing: check for proper phone number(?) (need some kind of standard format)
+            if request.form['address-2'] == '':
+                sql = ('insert into user_info (full_name, email, password, address_line_1, postal_code, phone_number) values (\'' +
+                        request.form['fullname'] + '\',\'' + request.form['email'] + '\',\'' +
+                        request.form['password'] + '\',\'' + request.form['address-1'] + '\', \'' + request.form['postal'] + '\',\'' + request.form['phone-number'] + '\')')
+            else:
+                sql = ('insert into user_info (full_name, email, password, address_line_1, address_line_2, postal_code, phone_number) values (\'' +
+                        request.form['fullname'] + '\',\'' + request.form['email'] + '\',\'' +
+                        request.form['password'] + '\',\'' + request.form['address-1'] + '\',\'' +
+                        request.form['address-2'] + '\',\'' + request.form['postal'] + '\',\'' + request.form['phone-number'] + '\')')
+            msg = write_db(sql)
 
-        if type(msg) == int:
-            return redirect(url_for('login', f='register'))
-        elif 'constraint failed' in msg and 'user_info.email' in msg:
-            error = 'An existing account is associated with the email provided. If you have forgotten your password, please recover your password.'
+            if type(msg) == int:
+                register_passed = True
+                BUILD_USER = request.form['email']
+                render_template('register.html', error=error, register_passed=register_passed)
+                # return redirect(url_for('login', f='register'))
+            elif 'constraint failed' in msg and 'user_info.email' in msg:
+                error = 'An existing account is associated with the email provided. If you have forgotten your password, please recover your password.'
+            else:
+                error = 'An unknown error has occured, please contact the site admin.'
         else:
-            error = 'An unknown error has occured, please contact the site admin.'
-    
-    return render_template('register.html', title='CYPRESS- Register', error = error)
+            # set security questions, give option to skip for now
+            sql = ('update user_info set security_question_1 =\'' + request.form['security1'] + '\', security_answer_1 =\'' + 
+                    request.form['security_answer_1'] + '\', security_question_2 =\'' + request.form['security2'] + '\', security_answer_2 =\'' +
+                    request.form['security_answer_2'] + '\', security_question_3 =\'' + request.form['security3'] + '\', security_answer_3 =\'' + request.form['security_answer_3'] + '\' where email=\'' + BUILD_USER + '\'')
+            msg = write_db(sql)
+            BUILD_USER = ""
+            if type(msg) == int:
+                return redirect(url_for('login', f='register'))
+
+    return render_template('register.html', title='CYPRESS- Register', error = error, register_passed=register_passed)
+
 
 @app.route('/logout')
 def logout():
@@ -133,9 +168,56 @@ def logout():
 	session.pop('user', None)
 	return redirect(url_for('login'))
 
-@app.route('/forgot_password')
+@app.route('/forgot_password', methods=['GET','POST'])
 def forgot_password():
-    return render_template('forgot_password.html', title='CYPRESS- Forgot Password')
+    error = ''
+    question = ""
+    wrong_ans = ""
+    found_email = False
+    success = False
+    global RECOVERY_ANS
+    global PASS_RECOVERY
+    global RECOVERY_QUESTION
+    # pick a question to ask the user for authentication
+    # assuming that the user has chosen and answered 3 security questions after registration
+    choose_1 = random.randint(1,3)
+    question_number = "security_question_" + str(choose_1)
+    answer_number = "security_answer_" + str(choose_1)
+
+    if request.method == 'POST':
+        if 'email' in request.form:
+            sql_q = 'select ' + question_number + ' from user_info where email=\'' + request.form['email'] + '\''
+            sql_a = 'select ' + answer_number + ' from user_info where email=\'' + request.form['email'] + '\''
+            try:
+                user_security_q = query_db(sql_q)[0]
+                user_security_a = query_db(sql_a)[0]
+                RECOVERY_QUESTION = SECURITY_QUESTIONS[int(user_security_q[0]-1)]
+                RECOVERY_ANS = user_security_a[0]
+                PASS_RECOVERY = query_db('select password from user_info where email=\'' + request.form['email'] + '\'')[0][0]
+                found_email = True
+            except IndexError:
+                error = 'The email provided is not associated with a CYPRESS account! Please try again.'
+                return render_template('forgot_password.html', error=error, question=RECOVERY_QUESTION, found=found_email, success=success, wrong_ans=wrong_ans)
+
+        else:
+            if request.form['security_a'].lower() == RECOVERY_ANS.lower():
+                # USER SUCCESSFULLY AUTHENTICATED SECURITY QUESTION!
+                # PROCEED TO EMAIL THEM THEIR PASSWORD
+                # <------------here----------->
+
+                # <-----------to here--------->
+                success = True
+                RECOVERY_QUESTION = ""
+                RECOVERY_ANS = ''
+                PASS_RECOVERY = ''
+            else:
+                wrong_ans = 'Incorrect security answer, please try again.'
+                found_email = True
+                return render_template('forgot_password.html', error=error, question=RECOVERY_QUESTION, found=found_email, success=success, wrong_ans=wrong_ans)
+
+
+
+    return render_template('forgot_password.html', title='CYPRESS- Forgot Password', error=error, question=RECOVERY_QUESTION, found=found_email, success=success, wrong_ans=wrong_ans)
 
 @app.route('/home')
 def home():
@@ -144,36 +226,49 @@ def home():
 
     return render_template('home.html')
 
-@app.route('/create_report')
+@app.route('/create_report', methods=['GET', 'POST'])
 def create_report():
-    return render_template('create_report.html', title = 'CYPRESS- Create a Report')
+    if request.method == "POST":
+        # write new report to db
+        # note: long_lat holds coordinates
+        if request.json['additional'] == '':
+            sql = ('insert into reports (long_lat, address, type_problem, report_author) values (\'' + request.json['loc'] + '\',\'' + request.json['address'] +
+                    '\', \'' + request.json['issues'] + '\',\'' + session['user']['email'] + '\')')
+        else:
+            sql = ('insert into reports (long_lat, address, type_problem, additional_details, report_author) values (\'' + request.json['loc'] + '\',\'' + request.json['address'] +
+                    '\', \'' + request.json['issues'] + '\',\'' + request.json['additional'] + '\',\'' + session['user']['email'] + '\')')
+        # create new report
+        print(sql)
+        write_db(sql)
+        # send confirmation
+    return render_template('create_report.html')
 
 @app.route('/edit_report')
 def edit_report():
-    return render_template('edit_report.html', title='CYPRESS- Edit Reports')
+    return render_template('edit_report.html')
 
 @app.route('/suggestions')
 def suggestions():
-    return render_template('suggestions.html', title='CYPRESS- Suggest Solutions')
+    return render_template('suggestions.html')
 
 @app.route('/faq')
 def faq():
-    return render_template('faq.html', title='CYPRESS- FAQ')
+    return render_template('faq.html')
 
 @app.route('/contact_us')
 def contact_us():
-    return render_template('contact_us.html', title='CYPRESS- Contact Us')
+    return render_template('contact_us.html')
 
 @app.route('/user_settings')
 def user_settings():
-    return render_template('user_settings.html', title='CYPRESS- User Settings')
+    return render_template('user_settings.html')
 
 @app.route('/tell_a_friend')
 def tell_a_friend():
-    return render_template('tell_a_friend.html',title='CYPRESS- Tell a Friend!')
+    return render_template('tell_a_friend.html')
 
-# WIP For Tables
-'''@app.route('/grades', methods=['GET', 'POST', 'DEL'])
+# For Reference ONLY
+@app.route('/grades', methods=['GET', 'POST', 'DEL'])
 def grades():
     #pull the entire grades table
     student_grades = query_db('select * from grades')
@@ -229,6 +324,7 @@ def grades():
 
         return render_template('grades.html', table_builder = table_builder, send_message = send_message)
 
+'''
 @app.route('/resources')
 def res():
     return render_template('resources.html', title='Resources')
